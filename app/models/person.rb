2610ff2ffda4313cd80c7bad855311e93db6e0e7
Josef Pospíšil <josef.pospisil@laststar.eu>
2023-03-20 19:28:15 +0100

    Add dependency to phones and emails

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,7 +4,8 @@ class Person < ApplicationRecord
     end
 
     has_many :addresses, dependent: :destroy
-
+    has_many :phones, dependent: :destroy
+    has_many :emails, dependent: :destroy
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :salutation, inclusion: { in: self.salutations,
